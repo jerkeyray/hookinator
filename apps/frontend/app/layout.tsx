7cf13e35aa056e6a9aebae6217fa6fcb64d8e214
srivastavya02 <srivastavya24@gmail.com>
2025-07-29 00:59:52 +0530

    added analytics

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,30 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Hookinator",
   description: "webhook testing app",
+  openGraph: {
+    title: "Hookinator",
+    description: "Webhook Testing Toolkit.",
+    url: "https://hookinator.jerkeyray.com",
+    siteName: "Hookinator",
+    images: [
+      {
+        url: "/hookinator.png", 
+        width: 1200,
+        height: 630,
+        alt: "A promotional image for the Hookinator application.",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hookinator",
+    description: "Webhook Testing Toolkit.",
+    // The image URL should be absolute for Twitter cards
+    images: ["https://hookinator.jerkeyray.com/hookinator.png"], 
+  },
+
 };
 
 export default function RootLayout({
@@ -29,6 +54,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>{children}</SessionProvider>
+        <Analytics />
       </body>
     </html>
   );
